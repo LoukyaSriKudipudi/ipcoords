7c5c743eea337a3ae71b92a5ecf58bb3d4f5497d
Loukya Sri <loukyasrikudipudi@gmail.com>
2025-08-01 07:09:38 +0530

    saveIpInfo.js

--- controllers/saveIpInfo.js
+++ controllers/saveIpInfo.js
@@ -3,8 +3,7 @@ const Iplog = require("./../models/ipLogModel");
 const saveIpInfo = async (data) => {
   try {
     const newIpLog = await Iplog.create(data);
-    console.log("✅ IP data saved to DB");
-  } catch (err) {
+    } catch (err) {
     console.error("❌ Failed to save IP data:", err.message);
   }
 };
